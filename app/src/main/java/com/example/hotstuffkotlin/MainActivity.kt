9c8b96b32d95df0d6011ab8a6672d6f5e1309ec4
Cam Patten <campatten@outlook.com>
2023-10-11 20:02:19 -0400

    Scaffolded out bottom drawer from Create logo.

--- app/src/main/java/com/example/hotstuffkotlin/MainActivity.kt
+++ app/src/main/java/com/example/hotstuffkotlin/MainActivity.kt
@@ -21,31 +21,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // appbar
         setSupportActionBar(findViewById(R.id.appBar))
 
+        // navbar
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_items,
-                R.id.navigation_create,
-                R.id.navigation_learn,
-                R.id.navigation_settings
-            )
-        )
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home,
+            R.id.navigation_items,
+            R.id.navigation_create,
+            R.id.navigation_learn,
+            R.id.navigation_settings))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-
+        // dropdown / autocomplete
         val languages = resources.getStringArray(R.array.programming_languages)
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
         val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
         autocompleteTV.setAdapter(arrayAdapter)
     }
-
-
 }
--- app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.hotstuffkotlin.databinding.FragmentSettingsBinding
@@ -26,13 +25,8 @@ class SettingsFragment : Fragment() {
             ViewModelProvider(this).get(SettingsViewModel::class.java)
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textSettings
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
