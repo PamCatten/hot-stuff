4739ecea23021023b1596d14c6bc6a77a79e561f
Cam Patten <campatten@outlook.com>
2023-10-16 20:22:17 -0400

    Navbar progress.

--- app/src/main/java/com/example/hotstuffkotlin/MainActivity.kt
+++ app/src/main/java/com/example/hotstuffkotlin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hotstuffkotlin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
@@ -8,6 +10,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -21,22 +24,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // bottom dialog
-        val buttonShow = findViewById<ImageButton>(R.id.buttonShow)
-        val navigation_placeholder =
-        buttonShow.setOnClickListener {
-            val bsDialog = BottomSheetDialog(this)
-            val view = layoutInflater.inflate(R.layout.bottom_dialog, null)
-            val buttonClose = view.findViewById<ImageButton>(R.id.dialog_button_close)
-
-            buttonClose.setOnClickListener {
-                bsDialog.dismiss()
-            }
-            bsDialog.setCancelable(true)
-            bsDialog.setContentView(view)
-            bsDialog.show()
-        }
 
         // appbar
         setSupportActionBar(findViewById(R.id.appBar))
@@ -53,12 +40,46 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        // bottom dialog
+        // attempt 2
+        val bottomNav = findViewById<BottomNavigationView>(R.id.mobile_navigation)
+        val menuItem = bottomNav.menu.findItem(R.id.navigation_placeholder)
+
+
+        val createNavButton = findViewById<ImageButton>(R.id.buttonShow)
+        createNavButton.setOnClickListener {
+            val bsDialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.bottom_dialog, null)
+            val buttonClose = view.findViewById<ImageButton>(R.id.dialog_button_close)
+
+            buttonClose.setOnClickListener {
+                bsDialog.dismiss()
+            }
+            bsDialog.setCancelable(true)
+            bsDialog.setContentView(view)
+            bsDialog.show()
+        }
+        // attempt 1
+//        val buttonShow = findViewById<ImageButton>(R.id.buttonShow)
+//
+//        buttonShow.setOnClickListener {
+//            val bsDialog = BottomSheetDialog(this)
+//            val view = layoutInflater.inflate(R.layout.bottom_dialog, null)
+//            val buttonClose = view.findViewById<ImageButton>(R.id.dialog_button_close)
+//
+//            buttonClose.setOnClickListener {
+//                bsDialog.dismiss()
+//            }
+//            bsDialog.setCancelable(true)
+//            bsDialog.setContentView(view)
+//            bsDialog.show()
+//        }
+
         // dropdown / autocomplete
         val languages = resources.getStringArray(R.array.programming_languages)
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
         val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
         autocompleteTV.setAdapter(arrayAdapter)
     }
-
 
 }
