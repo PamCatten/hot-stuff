b7c1bfe9153af78e02eb230ee7fcaf0ba6a65c4e
Cam Patten <campatten@outlook.com>
2023-10-20 21:08:35 -0400

    Preference screen progress, working, but margins / padding seem to be off?

--- app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
@@ -1,42 +1,14 @@
 package com.example.hotstuffkotlin.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceFragmentCompat
-import com.example.hotstuffkotlin.databinding.FragmentSettingsBinding
+import com.example.hotstuffkotlin.R
 import com.example.hotstuffkotlin.utils.SharedPreferenceHelper
 
 class SettingsFragment : PreferenceFragmentCompat() {
     private lateinit var sharedPreferenceHelper: SharedPreferenceHelper
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         sharedPreferenceHelper = SharedPreferenceHelper.getInstance(requireContext())
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- app/src/main/java/com/example/hotstuffkotlin/utils/SharedPreferenceHelper.kt
+++ app/src/main/java/com/example/hotstuffkotlin/utils/SharedPreferenceHelper.kt
@@ -1,4 +1,33 @@
 package com.example.hotstuffkotlin.utils
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
 class SharedPreferenceHelper {
+
+    companion object {
+
+        private const val WEATHER_PREF_TIME = "Weather pref time"
+        private const val WEATHER_FORECAST_PREF_TIME = "Forecast pref time"
+        private const val CITY_ID = "City ID"
+        private var prefs: SharedPreferences? = null
+        private const val LOCATION = "LOCATION"
+
+        @Volatile
+        private var instance: SharedPreferenceHelper? = null
+
+        fun getInstance(context: Context): SharedPreferenceHelper {
+            synchronized(this) {
+                val _instance = instance
+                if (_instance == null) {
+                    prefs = PreferenceManager.getDefaultSharedPreferences(context)
+                    instance = _instance
+                }
+                return SharedPreferenceHelper()
+            }
+        }
+    }
+    fun getSelectedThemePref() = prefs?.getString("theme_key", "")
+
 }
