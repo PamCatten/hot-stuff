a79f84f8de73df2d5a7f88b2a70fb9a6f89437a7
Cam Patten <campatten@outlook.com>
2023-10-31 16:52:08 -0400

    Began creating expand/contract functionality for learn cards.

--- app/src/main/java/com/example/hotstuffkotlin/ui/learn/LearnFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/learn/LearnFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
+import com.example.hotstuffkotlin.R
 import com.example.hotstuffkotlin.databinding.FragmentLearnBinding
 
 class LearnFragment : Fragment() {
@@ -21,10 +26,25 @@ class LearnFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val learnViewModel =
-            ViewModelProvider(this).get(LearnViewModel::class.java)
-
         _binding = FragmentLearnBinding.inflate(inflater, container, false)
+
+        val cardViewAbout = view?.findViewById<ViewGroup>(R.id.learn_about_card)
+        val dropdownAbout = view?.findViewById<ConstraintLayout>(R.id.learn_about_header)
+        val dropdownIconAbout = view?.findViewById<ImageView>(R.id.learn_about_dropdown_icon)
+        val groupAbout = view?.findViewById<Group>(R.id.learn_about_group)
+
+        dropdownAbout?.setOnClickListener{
+            if(groupAbout?.visibility == View.VISIBLE && cardViewAbout != null) {
+                TransitionManager.beginDelayedTransition(cardViewAbout, AutoTransition())
+                groupAbout.visibility = View.GONE
+                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown_close)
+            }
+            else if (groupAbout?.visibility == View.GONE && cardViewAbout != null) {
+                TransitionManager.beginDelayedTransition(cardViewAbout, AutoTransition())
+                groupAbout.visibility = View.VISIBLE
+                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown)
+            }
+        }
 
         return binding.root
     }
