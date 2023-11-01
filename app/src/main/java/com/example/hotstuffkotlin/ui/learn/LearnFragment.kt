90c68f953527248e82271fde9b3354032f41433a
Cam Patten <campatten@outlook.com>
2023-11-01 16:52:29 -0400

    Got expand/contract working. Found UI bug with cards sliding up prematurely before animation can complete.

--- app/src/main/java/com/example/hotstuffkotlin/ui/learn/LearnFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/learn/LearnFragment.kt
@@ -27,7 +27,47 @@ class LearnFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLearnBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.fragment_learn, container, false)
 
+        // walkthrough card
+        val cardViewWalkthrough = view?.findViewById<ViewGroup>(R.id.learn_walkthrough_card)
+        val dropdownWalkthrough = view?.findViewById<ConstraintLayout>(R.id.learn_walkthrough_header)
+        val dropdownIconWalkthrough = view?.findViewById<ImageView>(R.id.learn_walkthrough_dropdown_icon)
+        val groupWalkthrough = view?.findViewById<Group>(R.id.learn_walkthrough_group)
+
+        dropdownWalkthrough?.setOnClickListener{
+            if(groupWalkthrough?.visibility == View.VISIBLE && cardViewWalkthrough != null) {
+                TransitionManager.beginDelayedTransition(cardViewWalkthrough, AutoTransition())
+                groupWalkthrough.visibility = View.GONE
+                dropdownIconWalkthrough?.setImageResource(R.drawable.icon_dropdown)
+            }
+            else if (groupWalkthrough?.visibility == View.GONE && cardViewWalkthrough != null) {
+                TransitionManager.beginDelayedTransition(cardViewWalkthrough, AutoTransition())
+                groupWalkthrough.visibility = View.VISIBLE
+                dropdownIconWalkthrough?.setImageResource(R.drawable.icon_dropdown_close)
+            }
+        }
+
+        // resources card
+        val cardViewResources = view?.findViewById<ViewGroup>(R.id.learn_resources_card)
+        val dropdownResources = view?.findViewById<ConstraintLayout>(R.id.learn_resources_header)
+        val dropdownIconResources = view?.findViewById<ImageView>(R.id.learn_resources_dropdown_icon)
+        val groupResources = view?.findViewById<Group>(R.id.learn_resources_group)
+
+        dropdownResources?.setOnClickListener{
+            if(groupResources?.visibility == View.VISIBLE && cardViewResources != null) {
+                TransitionManager.beginDelayedTransition(cardViewResources, AutoTransition())
+                groupResources.visibility = View.GONE
+                dropdownIconResources?.setImageResource(R.drawable.icon_dropdown)
+            }
+            else if (groupResources?.visibility == View.GONE && cardViewResources != null) {
+                TransitionManager.beginDelayedTransition(cardViewResources, AutoTransition())
+                groupResources.visibility = View.VISIBLE
+                dropdownIconResources?.setImageResource(R.drawable.icon_dropdown_close)
+            }
+        }
+
+        // about card
         val cardViewAbout = view?.findViewById<ViewGroup>(R.id.learn_about_card)
         val dropdownAbout = view?.findViewById<ConstraintLayout>(R.id.learn_about_header)
         val dropdownIconAbout = view?.findViewById<ImageView>(R.id.learn_about_dropdown_icon)
@@ -37,16 +77,16 @@ class LearnFragment : Fragment() {
             if(groupAbout?.visibility == View.VISIBLE && cardViewAbout != null) {
                 TransitionManager.beginDelayedTransition(cardViewAbout, AutoTransition())
                 groupAbout.visibility = View.GONE
-                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown_close)
+                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown)
             }
             else if (groupAbout?.visibility == View.GONE && cardViewAbout != null) {
                 TransitionManager.beginDelayedTransition(cardViewAbout, AutoTransition())
                 groupAbout.visibility = View.VISIBLE
-                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown)
+                dropdownIconAbout?.setImageResource(R.drawable.icon_dropdown_close)
             }
         }
 
-        return binding.root
+        return view
     }
 
     override fun onDestroyView() {
