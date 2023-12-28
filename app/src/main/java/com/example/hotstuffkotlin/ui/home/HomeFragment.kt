05a525f627a07cdcf452c57061f044d22cf8f675
Cam Patten <112127155+PamCatten@users.noreply.github.com>
2023-12-27 19:21:36 -0500

    Home Fragment UI / Chart work.

--- app/src/main/java/com/example/hotstuffkotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.hotstuffkotlin.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,12 @@ import com.example.hotstuffkotlin.R
 import com.example.hotstuffkotlin.databinding.FragmentHomeBinding
 import com.example.hotstuffkotlin.utils.DatabaseHelper
 import com.example.hotstuffkotlin.utils.SharedPreferenceHelper
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.HorizontalBarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -34,6 +41,54 @@ class HomeFragment : Fragment() {
 //        val tvActiveBuildingName = view?.findViewById<TextView>(R.id.tvActiveBuildingName)
 //        val derivedName = "Sample House"
 //        tvActiveBuildingName?.text = derivedName
+
+        val distroChart = view.findViewById<BarChart>(R.id.topBarChart)
+        val list : ArrayList<BarEntry> = ArrayList()
+        list.add(BarEntry(1f, 100f))
+        list.add(BarEntry(2f, 101f))
+        list.add(BarEntry(3f, 103f))
+        list.add(BarEntry(4f, 105f))
+        list.add(BarEntry(5f, 104f))
+        list.add(BarEntry(6f, 102f))
+        val barDataSet = BarDataSet(list, "Category by Quantity")
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+        val barData = BarData(barDataSet)
+        distroChart.setFitBars(true)
+        distroChart.data = barData
+//        distroChart.description.text="Item Distribution by Category"
+        distroChart.axisRight.setDrawGridLines(false)
+        distroChart.axisLeft.setDrawGridLines(false)
+        distroChart.xAxis.setDrawGridLines(false)
+
+        val valueChart = view.findViewById<HorizontalBarChart>(R.id.bottomBarChart)
+        val valueList : ArrayList<BarEntry> = ArrayList()
+        valueList.add(BarEntry(1f, 106f))
+        valueList.add(BarEntry(2f, 101f))
+        valueList.add(BarEntry(3f, 104f))
+        valueList.add(BarEntry(4f, 103f))
+        valueList.add(BarEntry(5f, 105f))
+        valueList.add(BarEntry(6f, 102f))
+        valueList.add(BarEntry(7f, 106f))
+        valueList.add(BarEntry(8f, 107f))
+        valueList.add(BarEntry(9f, 102f))
+        valueList.add(BarEntry(10f, 104f))
+        valueList.add(BarEntry(11f, 103f))
+
+        val valueAxisLabels : ArrayList<String> = ArrayList()
+        valueAxisLabels.add("")
+        valueChart.xAxis.valueFormatter.
+
+        val valueBarDataSet = BarDataSet(valueList, "Room by Total Item Value")
+        valueBarDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+        valueBarDataSet.valueTextColor = Color.RED
+        val valueBarData = BarData(valueBarDataSet)
+        valueChart.setFitBars(true)
+        valueChart.data = valueBarData
+        valueChart.description.text = ""
+//        valueChart.description.text="Value by Room"
+        valueChart.axisRight.setDrawGridLines(false)
+        valueChart.axisLeft.setDrawGridLines(false)
+        valueChart.xAxis.setDrawGridLines(false)
 
         return binding.root
     }
