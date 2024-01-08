ad0ade67266863139aae9f48bfdfae61cc9305d8
Cam Patten <112127155+PamCatten@users.noreply.github.com>
2024-01-08 17:03:55 -0500

    I am a genius. Three birds, one stone today. Toolbar's working now, even if the solution is imperfect. Called SettingsFragment into a container fragment so I could remove the hack-y empty preference category technique to support landscape mode on devices. And lastly the Settings page finally has a real layout to make theming consistent with the rest of the app.

--- app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/settings/SettingsFragment.kt
@@ -1,78 +1,27 @@
 package com.example.hotstuffkotlin.ui.settings
 
-import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.MenuProvider
-import androidx.lifecycle.Lifecycle
-import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import com.example.hotstuffkotlin.R
 import com.example.hotstuffkotlin.utils.SharedPreferenceHelper
 
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var preferenceHelper: SharedPreferenceHelper
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        // TODO: Obliterates DRY, cannot customize menu items visibility w/ activity based menu providers, find another workaround
-        requireActivity().addMenuProvider(object: MenuProvider {
-            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                if (!menu.hasVisibleItems()) menuInflater.inflate(R.menu.menu_toolbar_main, menu)
-            }
-            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                when (menuItem.itemId) {
-                    R.id.toolbar_main_search -> {
-                        findNavController().navigate(R.id.navigation_search)
-                        return true
-                    }
-                    R.id.toolbar_main_report -> {
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.toolbar_issue_link))))
-                        return true
-                    }
-                    R.id.toolbar_main_rate -> { return true }
-                    R.id.toolbar_main_feedback -> {
-                        val intent = Intent(Intent.ACTION_VIEW)
-                        // TODO: Find an alternative way to extract these
-                        intent.data = Uri.parse(
-                            "mailto:campatten.dev@outlook.com" +
-                                    "?subject=FEEDBACK: (Your Suggestion)" +
-                                    "&body=Hey! Thanks for helping me improve Hot Stuff. Just a quick heads up, please make sure 'feedback' is somewhere in the subject of your suggestion so it ends up where I can see it! \n\n Much love, \nCam"
-                        )
-                        startActivity(intent)
-                        return true
-                    }
-                    else -> return true
-                }
-            }
-            override fun onPrepareMenu(menu: Menu) {
-                menu.findItem(R.id.toolbar_main_search).setVisible(false)
-                menu.findItem(R.id.toolbar_main_report).setVisible(true)
-                menu.findItem(R.id.toolbar_main_rate).setVisible(true)
-                menu.findItem(R.id.toolbar_main_feedback).setVisible(true)
-            }
-        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
-    }
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         preferenceHelper = SharedPreferenceHelper.getInstance(requireContext())
     }
-
     override fun onResume() {
         super.onResume()
         preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
     }
-
     override fun onPause() {
         super.onPause()
         preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
-
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
         when (key) {
             "theme" -> {
