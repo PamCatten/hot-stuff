5bc3eb63b2a10fce98675f613f34acb076a4c82c
Cam Patten <112127155+PamCatten@users.noreply.github.com>
2024-02-14 15:21:37 -0500

    Add ColorTemplate documentation and update chart theme to support twenty elements.

--- app/src/main/java/com/example/hotstuffkotlin/utils/ColorTemplate.kt
+++ app/src/main/java/com/example/hotstuffkotlin/utils/ColorTemplate.kt
@@ -1,18 +1,33 @@
 package com.example.hotstuffkotlin.utils
 import android.graphics.Color
-
+import com.github.mikephil.charting.data.BarDataSet
 object ColorTemplate {
+    /**
+     * Contains array of RGB colors used in HomeFragment charts
+     *
+     * @see [BarDataSet.setColors]
+     * @author Cam Patten
+     */
     val CHART_THEME = intArrayOf(
-        Color.rgb(255, 173, 154), //#ffad9a
-        Color.rgb(194, 37, 40), //#c22528
-        Color.rgb(255, 143, 126), //#ff8f7e
-        Color.rgb(169, 9, 23), //#a90917
-        Color.rgb(254, 109, 96), //#fe6d60
-        Color.rgb(140, 0, 4), //#8c0004
-        Color.rgb(239, 81, 73), //#ef5149
-        Color.rgb(110, 0, 0), //#6e0000
-        Color.rgb(216, 60, 56), //#d83c38
-        Color.rgb(86, 0, 0), //#510000
+        Color.rgb(255, 187, 167), // #ffbba7
+        Color.rgb(255, 173, 153), // #ffad99
+        Color.rgb(255, 158, 139), // #ff9e8b
+        Color.rgb(255, 142, 125), // #ff8e7d
+        Color.rgb(255, 125, 110), // #ff7d6e
+        Color.rgb(255, 107, 94), // #ff6b5e
+        Color.rgb(250, 91, 81), // #fa5b51
+        Color.rgb(239, 81, 72), // #ef5148
+        Color.rgb(227, 71, 64), // #e34740
+        Color.rgb(216, 60, 56), // #d83c38
+        Color.rgb(205, 49, 48), // #cd3130
+        Color.rgb(193, 36, 40), // #c12428
+        Color.rgb(182, 20, 32), // #b61420
+        Color.rgb(170, 6, 23), // #aa0617
+        Color.rgb(155, 0, 13), // #9b000d
+        Color.rgb(124, 0, 0), // #7c0000
+        Color.rgb(109, 0, 0), // #6d0000
+        Color.rgb(95, 0, 0), // #5f0000
+        Color.rgb(81, 0, 0), // #510000
     )
 
     // Saving resources used here for the README writeup
