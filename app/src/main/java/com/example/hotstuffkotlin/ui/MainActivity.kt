7c65c0176d5ff50ff07904c2087a46703f47e2b3
Cam Patten <112127155+PamCatten@users.noreply.github.com>
2024-02-16 16:52:38 -0500

    Fix up navigation in fragments.

--- app/src/main/java/com/example/hotstuffkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/hotstuffkotlin/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.hotstuffkotlin.R
@@ -16,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toolbar: Toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // navbar
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(setOf(
+        appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home,
             R.id.navigation_items,
             R.id.navigation_create_item,
@@ -39,6 +41,10 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_settings
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
+        toolbar.setNavigationOnClickListener { _ ->
+            NavigationUI.navigateUp(navController, appBarConfiguration)
+        }
+
         navView.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
@@ -51,37 +57,6 @@ class MainActivity : AppCompatActivity() {
                 else -> navView.visibility = View.GONE
             }
         }
-
-        // bottom dialog
-//        val createButton = navView.menu.findItem(R.id.navigation_placeholder)
-//        createButton.setOnMenuItemClickListener {
-//            val intent = Intent(this, NewItemActivity::class.java)
-//            startActivity(intent)
-//            navController.navigate(R.id.navigation_new_item)
-//            true
-//        }
-
-//        createButton.setOnMenuItemClickListener {
-//            val bsDialog = BottomSheetDialog(this)
-//            val view = layoutInflater.inflate(R.layout.bottom_dialog, null)
-//            val buttonClose = view.findViewById<ImageButton>(R.id.dialog_button_close)
-//            val buttonNewItem = view.findViewById<LinearLayout>(R.id.dialog_layoutNewItem)
-//            val buttonNewBuilding = view.findViewById<LinearLayout>(R.id.dialog_layoutNewBuilding)
-
-//            buttonClose.setOnClickListener { bsDialog.dismiss() }
-
-//            buttonNewItem.setOnClickListener {
-//                startActivity(Intent(this, NewItemActivity::class.java))
-//            }
-//            buttonNewBuilding.setOnClickListener {
-//                startActivity(Intent(this, NewBuildingActivity::class.java))
-//            }
-
-//            bsDialog.setCancelable(true)
-//            bsDialog.setContentView(view)
-//            bsDialog.show()
-//            true
-//        }
     }
 
     private fun initTheme() {
