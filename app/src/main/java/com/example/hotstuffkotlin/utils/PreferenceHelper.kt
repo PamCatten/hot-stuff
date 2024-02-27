eb84991a7c1e7bc3fc997d33e786520278da9356
Cam Patten <112127155+PamCatten@users.noreply.github.com>
2024-02-27 13:44:07 -0500

    Add KDocs for PreferenceHelper functions

--- app/src/main/java/com/example/hotstuffkotlin/utils/PreferenceHelper.kt
+++ app/src/main/java/com/example/hotstuffkotlin/utils/PreferenceHelper.kt
@@ -12,18 +12,53 @@ import com.example.hotstuffkotlin.R
  */
 class PreferenceHelper(val context: Context) {
     private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-    fun putBooleanPref(key: String, value:Boolean = false) {
+    /**
+     * Set a Boolean shared preference value.
+     * @param key The name of the preference to be set.
+     * @param value The new value for the preference.
+     * @author Cam Patten
+     */
+    fun putBooleanPref(key: String, value: Boolean = false) {
         prefs!!.edit().putBoolean(key, value).apply()
     }
+    /**
+     * Set a String shared preference value.
+     * @param key The name of the preference to be set.
+     * @param value The new value for the preference.
+     * @author Cam Patten
+     */
     fun putStringPref(key: String, value: String? = null) {
         prefs!!.edit().putString(key, value).apply()
     }
+    /**
+     * Retrieve a shared preference value of type String.
+     * @param key The name of the preference to be retrieved.
+     * @param defValue The value to be returned if the preference does not exist.
+     * @return
+     * Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with the key that is not a String.
+     * @author Cam Patten
+     */
     fun getStringPref(key: String, defValue: String? = ""): String? {
         return prefs!!.getString(key, defValue)
     }
+    /**
+     * Retrieve a shared preference value of type Boolean.
+     * @param key The name of the preference to be retrieved.
+     * @param defValue The value to be returned if the preference does not exist.
+     * @return
+     * Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with the key that is not a Boolean.
+     * @author Cam Patten
+     */
     fun getBooleanPref(key: String, defValue: Boolean = false): Boolean {
         return prefs!!.getBoolean(key, defValue)
     }
+    /**
+     * Sets the given shared preference value as the default theme for the activity.
+     * @param themePreference The type of theme.
+     * @author Cam Patten
+     */
     fun applyTheme(themePreference: String?) {
         when (themePreference) {
             context.getString(R.string.theme_light) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -32,7 +67,11 @@ class PreferenceHelper(val context: Context) {
             else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         }
     }
-
+    /**
+     * Retrieves the matching unicode currency icon for the given shared preference value.
+     * @param currency The type of currency.
+     * @author Cam Patten
+     */
     fun getCurrencyIcon(currency: String?): String {
         val icon = when (currency) {
             context.getString(R.string.cur_bitcoin) -> R.string.icon_bitcoin
